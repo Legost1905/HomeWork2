97a66811c548c2c2d4ec17eed7eae6533a66e2af
Legost1905 <ddd333.ru@gmail.com>
2022-11-29 16:26:36 +0300

    Third Commit

--- myapplication/src/main/java/com/example/myapplication/ui/main/MainViewModel.kt
+++ myapplication/src/main/java/com/example/myapplication/ui/main/MainViewModel.kt
@@ -5,6 +5,11 @@ import com.example.myapplication.businesslayer.CatProvider
 import com.example.myapplication.datalayer.IAccessor
 
 class MainViewModel : ViewModel() {
+
+
+
+
+
     private val accessor = IAccessor.create()
     private val provider = CatProvider(accessor)
     suspend fun getCats() = provider.getCats(0, 100)
