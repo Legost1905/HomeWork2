9ce37d4e5e0683fc45d3f9bbb09f3303fd19d161
Legost1905 <ddd333.ru@gmail.com>
2022-11-29 18:23:34 +0300

    Final version

--- myapplication/src/main/java/com/example/myapplication/ui/main/MainFragment.kt
+++ myapplication/src/main/java/com/example/myapplication/ui/main/MainFragment.kt
@@ -6,6 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
@@ -13,6 +16,7 @@ import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.example.myapplication.MainActivity
 import com.example.myapplication.R
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
@@ -41,29 +45,58 @@ class MainFragment : Fragment() {
         }
 
         val stub = view.findViewById<TextView>(R.id.stub)
+        val retryButton = view.findViewById<Button>(R.id.retry_button)
+        val retryLayout = view.findViewById<LinearLayout>(R.id.retry_layout)
+        val progress = view.findViewById<ProgressBar>(R.id.progress)
 
-        viewLifecycleOwner.lifecycleScope.launch {
-            stub.isVisible = true
-            stub.text="Загрузка..."
-//            delay(1000)
-            stub.setOnClickListener(null)
+        viewModel.state.observe(viewLifecycleOwner){
+            when(it){
+                is MainViewModel.State.loading -> {
+                    progress.isVisible = true
+                    retryLayout.isVisible = false
+                }
+                is MainViewModel.State.Success->{
+                    catAdapter.submitList(it.data)
+                    retryLayout.isVisible = false
+                    progress.isVisible = false
+                }
+                is MainViewModel.State.Error ->{
+                    retryLayout.isVisible = true
+                    progress.isVisible = true
 
-            try {
-                val list = withContext(Dispatchers.IO){viewModel.getCats()}
-                catAdapter.submitList(list)
-
-                stub.isVisible = false
-            }catch (error:Throwable){
-                stub.isVisible = true
-                stub.text="Ошибка: ${error.message}"
-                error.printStackTrace()
-                stub.setOnClickListener{
-                    //retry
+                    stub.text="Ошибка: ${it.throwable}"
+                    retryButton.setOnClickListener{
+                        viewModel.load()
+                    }
                 }
             }
-
-
         }
+
+        viewModel.load()
+
+
+//        viewLifecycleOwner.lifecycleScope.launch {
+//            stub.isVisible = true
+//            stub.text="Загрузка..."
+////            delay(1000)
+//            stub.setOnClickListener(null)
+//
+//            try {
+//                val list = withContext(Dispatchers.IO){viewModel.getCats()}
+//                catAdapter.submitList(list)
+//
+//                stub.isVisible = false
+//            }catch (error:Throwable){
+//                stub.isVisible = true
+//                stub.text="Ошибка: ${error.message}"
+//                error.printStackTrace()
+//                stub.setOnClickListener{
+//                    //retry
+//                }
+//            }
+//
+//
+//        }
     }
 
     companion object {
--- myapplication/src/main/java/com/example/myapplication/ui/main/MainViewModel.kt
+++ myapplication/src/main/java/com/example/myapplication/ui/main/MainViewModel.kt
@@ -1,16 +1,43 @@
 package com.example.myapplication.ui.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.recyclerview.widget.RecyclerView.State
 import com.example.myapplication.businesslayer.CatProvider
 import com.example.myapplication.datalayer.IAccessor
+import com.example.myapplication.objects.Cat
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
 
+    val state:MutableLiveData<State> = MutableLiveData()
 
-
-
+    val scope = CoroutineScope(Dispatchers.IO)
 
     private val accessor = IAccessor.create()
     private val provider = CatProvider(accessor)
     suspend fun getCats() = provider.getCats(0, 100)
+
+    fun load(){
+        state.postValue(State.loading)
+
+        scope.launch {
+            try{
+                val l = getCats()
+                state.postValue(State.Success(l))
+            }catch (error:Throwable){
+                state.postValue(State.Error(error))
+            }
+        }
+    }
+
+    sealed class State{
+        object loading:State()
+        class Success(val data: List<Cat>):State()
+        class Error(val throwable: Throwable):State()
+    }
+
 }
